8d873978a6e860e7778110b67029d531d7226f54
bishal-dd <dhakalbishal930@gmail.com>
2023-11-07 16:35:18 +0600

    apollo added back

--- server.js
+++ server.js
@@ -1,28 +1,35 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
+const { ApolloServer } = require("apollo-server-express");
 const cors = require("cors");
 var { graphqlHTTP } = require("express-graphql");
 var { buildSchema, GraphQLSchema } = require("graphql");
 const { expressjwt: jwt } = require("express-jwt");
 const db = require("./models");
-const schema = require("./schemas/index");
+const schema = require("./api/graphql");
 const port = 8080;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 require("dotenv").config();
 
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema,
-    graphiql: true,
-  })
-);
+const server = new ApolloServer({
+  schema,
+  context: ({ req }) => {
+    // You can add context data here if needed
+  },
+});
 
-db.sequelize.sync().then((req) => {
+async function startServer() {
+  await server.start();
+  server.applyMiddleware({ app, path: "/graphql" });
+
   app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
+}
+
+db.sequelize.sync().then(() => {
+  startServer();
 });
