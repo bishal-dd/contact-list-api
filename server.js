4f3c4dcbd5c41b59f47c23e6495b00d77516ac5b
bishal-dd <dhakalbishal930@gmail.com>
2023-11-07 18:12:20 +0600

    allow origin

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ const db = require("./models");
 const schema = require("./api/graphql");
 const port = 8080;
 
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
