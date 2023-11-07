614c2e4eefead2eb0609d9a9a17ff0e0c746a085
bishal-dd <dhakalbishal930@gmail.com>
2023-11-07 18:25:44 +0600

    cors add

--- server.js
+++ server.js
@@ -10,10 +10,17 @@ const db = require("./models");
 const schema = require("./api/graphql");
 const port = 8080;
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+app.use(function (req, res, next) {
+  // Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
+  );
   next();
 });
+
 app.use(cors());
 
 app.use(express.json());
